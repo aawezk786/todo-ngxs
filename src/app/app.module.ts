fa166dc3795253340f65ddf34dd51df0a3e5e182
aawez <aawez-at-656579430762>
2022-08-03 18:45:06 +0530

    Update:TODO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { TodoState } from './store/states/todo.state';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
+    NgxsModule.forRoot([
+      TodoState
+    ]),
+    HttpClientModule,
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
